9dd5f87ca664ac830fb37760da2b7abdf6d82ac2
Vinay <brvkr5794@gmail.com>
2024-05-24 01:03:50 -0500

    added .gitignore and initialized Spring Boot server

---
+++ server/src/main/java/com/example/chess/Main.java
@@ -0,0 +1,21 @@
+package com.example.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
